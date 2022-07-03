925d55a1adbe91768e0a6cb554063dc150168ff8
Ridho Dwi Ramadhan <minatoramakaze@gmail.com>
2022-07-03 15:25:29 +0700

    Initial commit

---
+++ resources/views/user/password.blade.php
@@ -0,0 +1,41 @@
+@extends('app')
+@section('content')
+<div class="row">
+    <div class="col-md-6">
+        @if (session('success'))
+        <p class="alert alert-success">{{ session('success') }}</p>
+        @endif
+        @if ($errors->any())
+        @foreach ($errors->all() as $err)
+        <p class="alert alert-danger">{{ $err }}</p>
+        @endforeach
+        @endif
+        <form method="POST" action="{{ route('password.action') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="">
+                    Old Password <span class="text-danger">*</span>
+                </label>
+                <input class="form-control" type="password" name="old_password"/>
+            </div>
+            <div class="mb-3">
+                <label for="">
+                    New Password <span class="text-danger">*</span>
+                </label>
+                <input class="form-control" type="password" name="new_password"/>
+            </div>
+            <div class="mb-3">
+                <label for="">
+                    New Password Confirmation <span class="text-danger">*</span>
+                </label>
+                <input class="form-control" type="password" name="new_password_confirmation"/>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary">Change Password</button>
+                <a class="btn btn-danger" href="{{ route('home') }}">Back</a>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
